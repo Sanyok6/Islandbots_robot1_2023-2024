1efe98ff23cd92e7402bffefa8935263a091b8c0
Sanyok6 <>
2024-01-06 13:41:57 -0500

    Use team prop detector data to place purple pixel in the correct location.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AUTO_RED_RIGHT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AUTO_RED_RIGHT.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TeamPropDetector;
@@ -27,6 +32,15 @@ public class AUTO_RED_RIGHT extends LinearOpMode {
                 hardwareMap.get(DistanceSensor.class, "fr_dist"));
         detector.readPosition();
 
+        // initialize servos
+        CRServo li_servo = hardwareMap.crservo.get("li_servo");
+        CRServo ri_servo = hardwareMap.crservo.get("ri_servo");
+        li_servo.setPower(0);
+        ri_servo.setPower(0);
+
+        Servo intake_lift = hardwareMap.servo.get("intake_lift");
+        intake_lift.setPosition(0);
+
         // generate trajectories
 
         // start by aligning to 2nd team prop location
@@ -40,12 +54,12 @@ public class AUTO_RED_RIGHT extends LinearOpMode {
                 .build();
 
         // then align to 1st team prop location
-        Action toPos1 = drive.actionBuilder(new Pose2d(35, -34, 0))
+        Action toPos1 = drive.actionBuilder(new Pose2d(35, -34, Math.toRadians(0)))
                 .lineToX(14)
                 .build();
 
         // finally, move the the backdrop
-        Action toBackdrop = drive.actionBuilder(new Pose2d(14, -34, 0))
+        Action toBackdrop = drive.actionBuilder(new Pose2d(14, -34, Math.toRadians(0)))
                 .lineToX(-51)
                 .build();
 
@@ -57,20 +71,52 @@ public class AUTO_RED_RIGHT extends LinearOpMode {
         }
         if (isStopRequested()) return;
 
+        // lower intake
+        intake_lift.setPosition(0.5);
+
+        // start movement
         Actions.runBlocking(
                 new SequentialAction(
                         toPos2,
-                        toPos3,
-                        toPos1,
+                        new outtakeIfAboveCorrectLocation(li_servo, ri_servo, 2, detector.position),
 
-                        (telemetryPacket) -> {
-                            telemetry.addLine("test");
-                            telemetry.update();
-                            return false;
-                        },
+                        toPos3,
+                        new outtakeIfAboveCorrectLocation(li_servo, ri_servo, 3, detector.position),
+
+                        toPos1,
+                        new outtakeIfAboveCorrectLocation(li_servo, ri_servo, 1, detector.position),
 
                         toBackdrop
                 )
         );
     }
+
+    // Places purple pixel in correct location
+    class outtakeIfAboveCorrectLocation implements Action {
+        public int currentPos;
+        public int teamPropPos;
+
+        public CRServo li_servo;
+        public CRServo ri_servo;
+
+        public outtakeIfAboveCorrectLocation(CRServo li_servo, CRServo ri_servo, int currentPos, int teamPropPos) {
+            this.currentPos = currentPos;
+            this.teamPropPos = teamPropPos;
+            this.li_servo = li_servo;
+            this.ri_servo = ri_servo;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (currentPos == teamPropPos) {
+                li_servo.setPower(1);
+                ri_servo.setPower(-1);
+                sleep(1000);
+                li_servo.setPower(0);
+                ri_servo.setPower(0);
+            }
+            return false;
+        }
+    }
+
 }
